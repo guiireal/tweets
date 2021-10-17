c0bd7e72b02e904684a220bb952eab5176716781
guiireal <guii_real@hotmail.com>
2021-10-17 00:21:14 -0300

    feat: configure show tweets and functionality

---
+++ app/Http/Livewire/ShowTweets.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Tweet;
+use Livewire\Component;
+
+class ShowTweets extends Component
+{
+    public $message = 'Apenas um teste';
+
+    public function render()
+    {
+        $tweets = Tweet::all();
+        return view('livewire.show-tweets', [
+            'tweets' => $tweets
+        ]);
+    }
+}
--- database/seeders/TweetSeeder.php
+++ database/seeders/TweetSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tweet;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class TweetSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([UserSeeder::class, TweetSeeder::class]);
+        Tweet::factory(3)->create();
     }
 }
